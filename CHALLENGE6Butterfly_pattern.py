70f8862fede04e8eaf624130250305bf72ce9a78
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-07-18 09:38:49 +0530

    Add files via upload

---
+++ CHALLENGE6Butterfly_pattern.py
@@ -0,0 +1,43 @@
+# CHALLENGE6 Butterfly pattern
+# print the given pattern
+
+def pattern(n):
+	
+	# This is upper
+	# half of pattern
+	for i in range(1, n + 1):
+		for j in range(1, 2 * n + 1):
+			
+			# Left part of pattern
+			if (i < j):
+				print("", end = " ");
+			else:
+				print("*", end = "");
+				
+			# Right part of pattern
+			if (i <= ((2 * n) - j)):
+				print("", end = " ");
+			else:
+				print("*", end = "");
+		print("");
+	
+	# This is lower
+	# half of pattern
+	for i in range(1, n + 1):
+		for j in range(1, 2 * n + 1):
+			
+			# Left part of pattern
+			if (i > (n - j + 1)):
+				print("", end = " ");
+			else:
+				print("*", end = "");
+			
+			# Right part of pattern
+			if ((i + n) > j):
+				print("", end = " ");
+			else:
+				print("*", end = "");
+		print("");
+
+n=int(input("enter n : "))
+pattern(n);
