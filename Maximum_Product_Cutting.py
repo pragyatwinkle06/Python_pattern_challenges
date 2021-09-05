ca5fdf80eabdc896b361fef9cf36ab02a24f43c6
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-09-05 08:23:30 +0530

    Update Maximum_Product_Cutting.py

--- Maximum_Product_Cutting.py
+++ Maximum_Product_Cutting.py
@@ -1,6 +1,28 @@
 # The main function that returns maximum
 # product obtainable from a rope of length n
 
+''' Maximum Product Cutting | DP-36
+Difficulty Level : Medium
+Last Updated : 14 May, 2021
+Given a rope of length n meters, cut the rope in different parts of integer lengths in a way that maximizes product of
+lengths of all parts. You must make at least one cut. Assume that the length of rope is more than 2 meters. 
+Examples: 
+
+Input: n = 2
+Output: 1 (Maximum obtainable product is 1*1)
+
+Input: n = 3
+Output: 2 (Maximum obtainable product is 1*2)
+
+Input: n = 4
+Output: 4 (Maximum obtainable product is 2*2)
+
+Input: n = 5
+Output: 6 (Maximum obtainable product is 2*3)
+
+Input: n = 10
+Output: 36 (Maximum obtainable product is 3*3*4)  '''
+
 def maxProd(n):
 	
 	# Base cases
